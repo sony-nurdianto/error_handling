f3e733566c40864ed8b65aff68140a6e0eca59a8
sony nurdianto <sonynurdianto445@gmail.com>
2024-07-09 11:57:06 +0700

    first example error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let open_file: io::Result<File> = File::open("hello.txt");
+
+    let mut file: File = match open_file {
+        Ok(file) => file,
+        Err(e) => panic!("Failed to Open file: {e}"),
+    };
+
+    let mut user_file: String = String::new();
+
+    match file.read_to_string(&mut user_file) {
+        Ok(val) => val,
+        Err(e) => panic!("Error to Read file {e}"),
+    };
+    println!("{}", user_file);
 }
