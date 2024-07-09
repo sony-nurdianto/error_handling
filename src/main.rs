f2a57c4fb7f53a42a657ee7886dfe4853c4b0c74
sony nurdianto <sonynurdianto445@gmail.com>
2024-07-09 13:32:54 +0700

    other error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
-    fs::File,
-    io::{self, Read},
+    fs::{self, File},
+    io::{self, ErrorKind, Read},
 };
 
 fn main() {
@@ -17,5 +17,36 @@ fn main() {
         Ok(val) => val,
         Err(e) => panic!("Error to Read file {e}"),
     };
+
     println!("{}", user_file);
+
+    let open_file_2: io::Result<File> = File::open("raja_mexico.txt");
+
+    let mut file_2 = match open_file_2 {
+        Ok(file) => file,
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => {
+                match File::create("raja_mexico.txt") {
+                    Ok(new_file) => {
+                        eprintln!("Failed to found file , file will be created");
+                        new_file
+                    }
+                    Err(_) => panic!("failed to created file , exit program !"),
+                };
+                fs::write("raja_mexico.txt", "Ini Aku Raja Mexico ").unwrap_or_else(|err| {
+                    panic!("success to create a file but failed to write: {err:?}, please Re run the program ");
+                });
+                panic!("success to create and write file. please re run the program");
+            }
+            _ => {
+                panic!("failed to open file ")
+            }
+        },
+    };
+
+    let mut content_file2: String = String::new();
+    file_2
+        .read_to_string(&mut content_file2)
+        .expect("Failed to Read String");
+    println!("{}", content_file2);
 }
